62288a3a211644e391d170fead722772b9898eba
René Kooi <rene@kooi.me>
2015-06-18 19:22:39 +0200

    build: use standard ExtPlug build procedure

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,15 @@ var gulp   = require('gulp')
 var babel  = require('gulp-babel')
 var rjs    = require('requirejs')
 var fs     = require('fs')
+var mkdirp = require('mkdirp')
+var del    = require('del')
+var runseq = require('run-sequence')
+
+var pluginName = 'extplug/meh-icons'
+
+gulp.task('clean-lib', function (cb) {
+  del('lib', cb)
+})
 
 gulp.task('babel', function () {
   return gulp.src('src/**/*')
@@ -9,28 +18,41 @@ gulp.task('babel', function () {
     .pipe(gulp.dest('lib/'))
 })
 
-gulp.task('rjs', [ 'babel' ], function (done) {
+gulp.task('rjs', function (done) {
+  // these paths are defined at runtime, so the r.js optimizer can't find them
+  var paths = {
+    // plug files, define()d by plug-modules
+    plug: 'empty:',
+    // extplug defines
+    extplug: 'empty:',
+    // plug.dj language files
+    lang: 'empty:',
+    // libraries used by plug.dj
+    backbone: 'empty:',
+    jquery: 'empty:',
+    underscore: 'empty:',
+    // libraries used by extplug
+    meld: 'empty:',
+    'plug-modules': 'empty:'
+  }
+
+  // location of the files for this plugin
+  paths[pluginName] = 'lib/'
+
   rjs.optimize({
     baseUrl: './',
-    name: 'extplug/meh-icons/main',
-    paths: {
-      // plug-modules defines, these are defined at runtime
-      // so the r.js optimizer can't find them
-      plug: 'empty:',
-      'extplug/meh-icons': 'lib/',
-      extplug: 'empty:',
-      lang: 'empty:',
-      backbone: 'empty:',
-      jquery: 'empty:',
-      underscore: 'empty:',
-      meld: 'empty:',
-      'plug-modules': 'empty:'
-    },
+    name: pluginName + '/main',
+    paths: paths,
     optimize: 'none',
     out: function (text) {
-      fs.writeFile('build/meh-icons.js', text, done)
+      mkdirp('build', function (e) {
+        if (e) done(e)
+        else   fs.writeFile('build/meh-icons.js', text, done)
+      })
     }
   })
 })
 
-gulp.task('build', [ 'rjs' ])
+gulp.task('build', function () {
+  return runseq('clean-lib', 'babel', 'rjs')
+})
