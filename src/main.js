1d6d3ec0123a008cd0b081cb4b8b8827cee9095c
René Kooi <renee@kooi.me>
2016-09-25 17:34:06 +0200

    honour "rules.allowShowingMehs" room setting

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Plugin from 'extplug/Plugin';
 import UserRowView from 'plug/views/rooms/users/RoomUserRowView';
 import $ from 'jquery';
 import { around } from 'meld';
+import defined from 'defined';
 import style from './style.css';
 
 const MehIcon = Plugin.extend({
@@ -12,28 +13,39 @@ const MehIcon = Plugin.extend({
   style,
 
   enable() {
-    this.advice = around(UserRowView.prototype, 'vote', this.showVote);
+    const plugin = this;
+    this.advice = around(UserRowView.prototype, 'vote', function boundToRow() {
+      plugin.showVote(this);
+    });
   },
 
   disable() {
     this.advice.remove();
   },
 
-  // bound to the UserRowView instance
+  shouldShowMehs() {
+    const user = API.getUser();
+    if (user && user.role >= API.ROLE.BOUNCER || user.gRole >= API.ROLE.BOUNCER) {
+      return true;
+    }
+    const rules = this.ext.roomSettings.get('rules');
+    return defined(rules && rules.allowShowingMehs, true);
+  },
+
   // shows all relevant vote icons instead of just grab or woot.
-  showVote() {
-    if (this.$icon) this.$icon.remove();
-    this.$icon = $();
-    if (this.model.get('vote') < 0) {
-      this.$icon = this.$icon.add($('<i />').addClass('icon icon-meh extplug-meh-icon'));
+  showVote(row) {
+    if (row.$icon) row.$icon.remove();
+    row.$icon = $();
+    if (row.model.get('vote') < 0 && this.shouldShowMehs()) {
+      row.$icon = row.$icon.add($('<i />').addClass('icon icon-meh extplug-meh-icon'));
     }
-    if (this.model.get('vote') > 0) {
-      this.$icon = this.$icon.add($('<i />').addClass('icon icon-woot'));
+    if (row.model.get('vote') > 0) {
+      row.$icon = row.$icon.add($('<i />').addClass('icon icon-woot'));
     }
-    if (this.model.get('grab')) {
-      this.$icon = this.$icon.add($('<i />').addClass('icon icon-grab'));
+    if (row.model.get('grab')) {
+      row.$icon = row.$icon.add($('<i />').addClass('icon icon-grab'));
     }
-    this.$icon.appendTo(this.$el);
+    row.$icon.appendTo(row.$el);
   },
 });
 
